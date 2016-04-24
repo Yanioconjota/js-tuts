f3ad38009c16a26c0266f69ecd83e0c585a3496d
Yanioconjota <janioisacura@gmail.com>
2016-04-24 12:42:39 -0300

    JS for cats examples added

--- js/droidedit.js
+++ js/droidedit.js
@@ -1,24 +1,82 @@
 var GetApp = function(){
 	var selectors = {
-		replaceFirst : 'pre.replace>p:first',
-		replaceLast  : 'pre.replace>p:last'
+		buttonClear  		 : 'button.clear',
+		replaceFirst 		 : 'pre.replace>p:first',
+		replaceLast  		 : 'pre.replace>p:last',
+		scoopsTxtWh	 	   : 'pre.scoops-while>p',
+		scoopsActWh	 		 : 'a.scoops-while',
+		scoopsTxtIf	 		 : 'pre.scoops-if>p',
+		scoopsTxtIfVal	 : 'input[name="insertScoops"]',
+		scoopsActIf	 		 : 'a.scoops-if'
 	};
 	var globals = {};
 	return {
 		init : function(){
 			app = this;
 			globals.dom = {};
-			globals.dom.replaceFirst = $(selectors.replaceFirst);
-			globals.dom.replaceLast = $(selectors.replaceLast);
+			globals.dom.buttonClear 	 = $(selectors.buttonClear);
+			globals.dom.replaceFirst 	 = $(selectors.replaceFirst);
+			globals.dom.replaceLast 	 = $(selectors.replaceLast);
+			globals.dom.scoopsTxtWh 	 = $(selectors.scoopsTxtWh);
+			globals.dom.scoopsActWh 	 = $(selectors.scoopsActWh);
+			globals.dom.scoopsTxtIf 	 = $(selectors.scoopsTxtIf);
+			globals.dom.scoopsActIf 	 = $(selectors.scoopsActIf);
+			globals.dom.scoopsTxtIfVal = $(selectors.scoopsTxtIfVal);
 
 			app.replacetext();
+			app.bindEvents();
 		},
+
+		bindEvents : function(){
+
+			app = this;
+			globals.dom.buttonClear.bind('click',function(){
+				$(this).parent().find('pre>p').html('');
+				console.log('this is cleared!');
+			});
+
+			globals.dom.scoopsActWh.bind('click',function(){
+				globals.dom.scoopsTxtWh.html('');
+				app.scoopsWhile();
+			});
+
+			globals.dom.scoopsActIf.bind('click',function(){
+				globals.dom.scoopsTxtIf.html('');
+				app.scoopsIf();
+			});
+
+		},
+
 
 		replacetext : function(){
 			var dogSentence 	 =  'Dogs are the bane of my existence';
 			var dogSentenceRep = dogSentence.replace('Dogs','Those blasted dogs');
 			globals.dom.replaceFirst.html(dogSentence);
 			globals.dom.replaceLast.html(dogSentenceRep);
+		},
+
+		scoopsWhile : function(){
+			var scoops = 5;
+			while (scoops > 0) {
+				globals.dom.scoopsTxtWh.append("Another scoop<br>");
+				scoops = scoops - 1;
+			}globals.dom.scoopsTxtWh.append("Life without icecream isn't the same");
+		},
+
+		scoopsIf : function(){
+			console.log(scoops);
+			var scoops = globals.dom.scoopsTxtIfVal.val();
+			if (scoops >= 5) {
+				globals.dom.scoopsTxtIf.append("That's a lot of ice cream");
+			} else if (scoops == 4) {
+				globals.dom.scoopsTxtIf.append('Eat faster, the ice cream is goint to melt!');
+			} else if (scoops == 3) {
+				globals.dom.scoopsTxtIf.append('Ice cream is running low');
+			} else if (scoops == 2) {
+				globals.dom.scoopsTxtIf.append('Going once');
+			} else if (scoops == 2) {
+				globals.dom.scoopsTxtIf.append('Going twice');
+			}
 		}
 	}
 }
--- js/main2.js
+++ js/main2.js
@@ -1,327 +1,40 @@
-var GetApp = function(){
-	
-	var selectors = {
+var myImage = document.getElementById("mainImage");
 
-		owlSlider 	: '#owl-demo',
+var myArray = ["img/001.jpg","img/002.jpg","img/003.jpg","img/004.jpg","img/005.jpg","img/006.jpg","img/007.jpg","img/008.jpg",];
 
-		myForm			: '#my-form',			
+var refresh = true;
 
-		email 	  	: '#exampleInputEmail1',
+// seta o valor do index
+var index = 0;
 
-		mailMsg 		: 'span.mail-msg',
+// funcao para trocar as imagens
+function changeImage(){
+  myImage.setAttribute("src", myArray[index]);
+  index++;
+  if (index >= myArray.length) {
+    index = 0;
+  };
 
-		nameMsg 		: 'span.name-msg',
-
-		name  			: '#exampleInputName1',
-
-		combo1 	  	: '#exampleInputCombo1',
-
-		combo2 	  	: '#exampleInputCombo2',
-
-		comboMsg		: 'span.combo-msg',
-
-		submitBtn		: 'button[type=button]',
-
-		message 		: '#message'
-
-	};
-
-	var globals = {};
-
-	var ejecutarAJAX = function(url,data,callback){
-		$.ajax({
-			url : url,
-			method : "POST",
-			data : "data",
-			dataType: "JSON",
-			success : function(respuesta){
-				return callback(respuesta);
-			},
-			error : function(){
-				callback(false);
-			}
-		});
-	};
-
-	var validateEmail = function (email){
-		var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
-		
-		return re.test(email);
-	};
-	
-	return {
-
-		init : function(){
-
-			app = this;
-
-			globals.dom = {};
-
-			globals.keyValues = {
-
-				combo1 : {
-					"value" : "id",
-					"innerHTML" : "nombre"
-				},
-
-				combo2 : {
-					"value" : "id",
-					"innerHTML" : "nombre"
-				}
-
-			};
-
-			globals.options = {
-				email  : false,
-				name   : false,
-				combo1 : false,
-				combo2 : false,
-			};
-
-			globals.dom.owlSlider 	= $(selectors.owlSlider);
-
-			globals.dom.myForm 			= $(selectors.myForm);
-
-			globals.dom.email 	 		= $(selectors.email);
-
-			globals.dom.mailMsg 		= $(selectors.mailMsg);
-
-			globals.dom.name 				= $(selectors.name);
-
-			globals.dom.nameMsg 		= $(selectors.nameMsg);
-
-			globals.dom.combo1 	 		= $(selectors.combo1);
-
-			globals.dom.combo2 	 		= $(selectors.combo2);
-
-			globals.dom.comboMsg 	 		= $(selectors.comboMsg);
-
-			globals.dom.submitBtn 		= $(selectors.submitBtn);
-
-			globals.dom.message 			= $(selectors.message);
-
-			app.bindEvents();
-
-		},
-
-		fillCombo : function(combo,respuestaArray,keyObjeto){ 
-
-			var app = this;
-
-			if (typeof respuestaArray == 'object' && respuestaArray.length > 0 && respuestaArray) {
-
-				combo.html('');
-				combo.append(
-					$("<option/>")
-								.val(0)
-								.html('Seleccione una opción')
-				);
-
-				for (var i in respuestaArray) {
-					
-					combo.append(
-
-						$("<option/>")
-							.val(respuestaArray[i][ keyObjeto.value ])
-							.html(respuestaArray[i][ keyObjeto.innerHTML ])
-						);
-					
-				}
-
-			} else {
-				combo.html('');
-				combo.append(
-					$("<option/>")
-								.val(0)
-								.html('Seleccione una opción')
-				);
-			}
-			
-		},
-
-		validate : function (){
-
-			app = this;
-
-			globals.dom.mailMsg.text("");
-
-			var email = globals.dom.email.val();
-
-			if(globals.dom.email.val() === 0 && globals.dom.email.attr('id') === 'exampleInputEmail1') {
-					lobals.dom.email.parent().addClass('has-error');
-					globals.dom.email.parent().removeClass('has-success');
-					globals.dom.mailMsg.text("");
-					respuesta = [];
-				
-					app.fillCombo(globals.dom.combo1,respuesta,globals.keyValues.combo1);
-				}
-
-			if (validateEmail(email)) {
-				globals.dom.mailMsg.text(email + " is valid :)");
-				globals.dom.email.parent().removeClass('has-error');
-				globals.dom.email.parent().addClass('has-success');
-				globals.options.email = true;
-				
-			} else {
-				globals.dom.mailMsg.text(email + " is not valid :(");
-				globals.dom.email.parent().addClass('has-error');
-				globals.dom.email.parent().removeClass('has-success');
-				globals.options.email = false;
-			}
-
-			// Validate Name
-
-			if(globals.dom.name.val() != 0 && globals.dom.name.attr('id') === 'exampleInputName1') {
-					globals.dom.name.parent().removeClass('has-error');
-					globals.dom.name.parent().addClass('has-success');
-					globals.dom.nameMsg.text("");
-					globals.options.name = true;
-				} else {
-					globals.dom.name.parent().addClass('has-error');
-					globals.dom.name.parent().removeClass('has-success');
-					globals.dom.nameMsg.text("Come on! You must have a name!");
-					globals.options.name = false;
-				}
-
-				// validate Combo 1
-
-				if(globals.dom.combo1.find('option:selected').val() === 0 ) {
-					globals.options.combo1 = false;
-					globals.dom.combo
-				} else {
-					globals.options.combo1 = true;
-				}
-
-				// validate Combo 2
-
-				if(globals.dom.combo2.find('option:selected').val() === 0 ) {
-					globals.options.combo2 = false;
-					globals.dom.combo
-				} else {
-					globals.options.combo2 = true;
-				}
-		},
-
-		bindEvents : function(){
-
-			var app = this;
-
-			globals.dom.message.hide();
-			// Combo 1 depends on mail change
-
-			globals.dom.email.keyup(function(){
-
-				var object = {
-					id : $(this).find('option:selected').val()
-				};
-
-				// if it's validated call ejecutarAjax
-				if(validateEmail(globals.dom.email.val())) {
-					globals.dom.email.parent().removeClass('has-error');
-					globals.dom.email.parent().addClass('has-success');
-					globals.dom.mailMsg.html(globals.dom.email.val() + " is valid :)");
-					ejecutarAJAX('json/option-combo1.json',object,function(respuesta){
-						app.fillCombo(globals.dom.combo1,respuesta,globals.keyValues.combo1);
-					});
-					globals.options.email = true;
-					globals.options.combo1 = true;
-				} else {
-					// Show error message
-					globals.dom.email.parent().addClass('has-error');
-					globals.dom.email.parent().removeClass('has-success');
-					globals.dom.mailMsg.html(globals.dom.email.val() + " is not valid :(");
-					respuesta = [];
-					globals.options.email = false;
-					globals.options.combo1 = false;
-					app.fillCombo(globals.dom.combo1,respuesta,globals.keyValues.combo1);
-				}
-			});
-
-			globals.dom.name.keyup(function(){
-				if(globals.dom.name.val() != 0 ) {
-					globals.dom.name.parent().removeClass('has-error');
-					globals.dom.name.parent().addClass('has-success');
-					globals.options.name = true;
-				} else {
-					
-					globals.dom.name.parent().addClass('has-error');
-					globals.dom.name.parent().removeClass('has-success');
-					globals.options.name = false;
-				}
-			});
-
-			// Combo 2 depends on combo 1 value
-
-			globals.dom.combo1.on('change',function(){
-
-				var object = {
-					id : $(this).find('option:selected').val()
-				};
-
-				if (globals.dom.combo1.val() == 0) {
-					respuesta = [];
-					app.fillCombo(globals.dom.combo2,respuesta,globals.keyValues.combo2);
-					globals.options.combo2 = false;
-				} else {
-					globals.options.combo2 = true;
-					ejecutarAJAX('json/option-combo2.json',object,function(respuesta){
-						app.fillCombo(globals.dom.combo2,respuesta,globals.keyValues.combo2);
-					});
-				}
-
-			});
-
-			globals.dom.submitBtn.on('click',function(){
-				
-				if (globals.options.email === true && globals.options.name === true && globals.options.combo1 === true && globals.options.combo2 === true) {
-
-					globals.dom.myForm.remove();
-
-					globals.dom.message.show();
-
-				}
-
-				// email
-
-				if(globals.options.email === false){
-					console.log('wrong email');
-				} else {
-					console.log('right email');
-				}
-
-				// name
-				if(globals.options.name === false){
-					console.log('wrong name');
-				} else {
-					console.log('right name');
-				}
-
-				// combo1
-				if(globals.options.combo1 === false){
-					console.log('wrong combo1');
-				} else {
-					console.log('right combo1');
-				}
-
-				// combo2
-				if(globals.options.combo2 === false){
-					console.log('wrong combo2');
-				} else {
-					console.log('right combo2');
-				}
-				app.validate();
-			});
-
-		}
-
-	};
-
+  if (refresh) {
+  	miIntervalo();
+  }
 }
 
-$(document).ready(function(){
+// eventos de interação com a troca das imagens
+myImage.onmouseover = function(){
+  refresh = false;  
+}
 
-  var app = GetApp();
+myImage.onmouseout = function(){
+  refresh = true; 
+}
 
-  app.init();
+myImage.onclick = changeImage;
 
-});
+function miIntervalo (){
+	setTimeOut (function(){
+		changeImage();
+	},
+	1000
+	) 
+}
